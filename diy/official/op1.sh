465eab01177aa21db0bf2476644ad5a5c4fb242d
cddcx <84568016+cddcx@users.noreply.github.com>
2022-12-08 21:57:14 +0800

    Update op1.sh

--- diy/official/op1.sh
+++ diy/official/op1.sh
@@ -4,7 +4,7 @@
 # jsjson@163.com 
 #=================================================
 ##添加自己的插件库
-echo "src-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" >>  feeds.conf.default
+echo "src-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" >> feeds.conf.default
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
 
